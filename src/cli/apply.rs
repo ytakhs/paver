05425647abc385b42262a79207a28ce69056d0ad
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-01-19 22:57:04 +0900

    Use yaml instead of toml

--- src/cli/apply.rs
+++ src/cli/apply.rs
@@ -1,6 +1,6 @@
 use crate::action::ActionStorage;
 use crate::builtins;
-use crate::error::Result;
+use crate::error::{Error, Result};
 
 pub struct Apply {
     filepath: String,
@@ -14,6 +14,14 @@ impl Apply {
     pub fn run(&self) -> Result<()> {
         let mut action_storage = ActionStorage::new();
         let key = "git".to_string();
+
+        let content =
+            std::fs::read_to_string(self.filepath.as_str()).or(Err(Error::CommandError))?;
+
+        let value: serde_yaml::Value =
+            serde_yaml::from_str(content.as_str()).or(Err(Error::CommandError))?;
+
+        dbg!("{}", value);
 
         action_storage.store(key.clone(), Box::new(builtins::git::Git::new()));
 
